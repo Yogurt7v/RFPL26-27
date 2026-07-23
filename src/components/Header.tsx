4adb44a96e0735abf5c53fa63f9721697a7adafb
Yogurt7v <egorov-2k@yandex.ru>
2026-07-23 21:47:18 +0300

    чуть улучшил визуал навигации

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,12 @@
 import { Link } from 'react-router-dom'
-// import { useAuth } from '../hooks/useAuth'
-// import { LogoutIcon } from './Icons'
 
 export function Header() {
-  // const { user, logout } = useAuth()
-
   return (
     <header className="header">
       <Link to="/" className="header__logo">
-        <img src="/favicon/favicon-96x96.png" alt="РПЛ" className="header__logo-img" />
-        <span>РФПЛ Прогнозы</span>
+        <img src="/favicon/favicon-96x96.png" alt="Рфпл" className="header__logo-img" />
+        <span>Рфпл Прогнозы</span>
       </Link>
-
-      <div className="header__right">
-        {/*{user && (
-          <>
-            <span className="header__username">{user.username}</span>
-            <button className="header__logout" onClick={logout} title="Выйти">
-              <LogoutIcon size={20} />
-            </button>
-          </>
-        )}*/}
-      </div>
     </header>
   )
 }
